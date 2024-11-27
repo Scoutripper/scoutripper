78297be1e7275cc55badec170b3b9e133f82e81d
Scoutripper <scoutripper90@gmail.com>
2024-11-27 17:50:21 +0000

    final

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Compare from "./components/Compare";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <>
+      <div>
+        <Navbar />
+        <Compare />
+        <Footer />
+      </div>
+    </>
+  );
+}
+
+export default App;
